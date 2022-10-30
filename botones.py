39e150cf698537d65ec35110f80310b517368596
Luceth Argote <97134341+bl8dyg1rl@users.noreply.github.com>
2022-10-30 16:34:33 -0500

    Update botones.py

--- botones.py
+++ botones.py
@@ -1,21 +1,9 @@
-import pygame
-from director import screen
-
-#load buttons images
-start_img = pygame.image.load("btns/btn_jugar.png").convert_alpha()
-help_img = pygame.image.load("btns/btn_ayuda.png").convert_alpha()
-configuration_img = pygame.image.load("btns/btn_configuracion.png").convert_alpha()
-
 class Button():
     def __init__(self, x, y, image):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         
-    def draw(self):
+    def draw_button(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
         
-#create buttons instances
-start_btn = Button(100, 200, start_img)
-help_btn = Button(450, 200, help_img)
-configuration_btn = Button(304, 125, configuration_img)
