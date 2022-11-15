e845f3f57c87ac9a026f827b2c1cff2c7d488f21
JuanJoZP <juanzpjose136@gmail.com>
2022-11-14 21:42:26 -0500

    bounces

--- elements/duck.py
+++ elements/duck.py
@@ -1,6 +1,6 @@
 import pygame
 from random import randrange
-from math import sin, cos, radians
+from math import sin, cos, radians, pi
 
 from config import MAX_DUCK_SPAWN
 from utils import relative_pos
@@ -19,7 +19,7 @@ class Duck(Element):
         self.leave = False
         self.fall = False
 
-        self.angle = 45  # 0 a 360, inicializarlo random
+        self.angle = radians(30)  # 0 a 360 pero en radianes, inicializarlo random
         self.velocity = vel
 
     def on_update(self) -> None:
@@ -30,18 +30,31 @@ class Duck(Element):
         else:
             self.move_vel()
 
-    def on_draw(self, screen: pygame.Surface):
-        super().on_draw(screen)
+    def check_bounce(self):
+        MAX_X, MAX_Y = relative_pos(MAX_DUCK_SPAWN[0], MAX_DUCK_SPAWN[1])
+        if self.rect.top < 0:
+            self.angle = 2 * pi - self.angle
+            self.rect.top = 0
+        elif self.rect.bottom > MAX_Y:
+            self.angle = 2 * pi - self.angle
+            self.rect.bottom = MAX_Y
+        elif self.rect.left < 0:
+            self.angle = pi - self.angle
+            self.rect.left = 0
+        elif self.rect.right > MAX_X:
+            self.angle = pi - self.angle
+            self.rect.right = MAX_X
 
     def move_vel(self):
-        # FALTA HACER LOS REBOTES
-        x = self.velocity * cos(radians(self.angle))
+        self.check_bounce()
+        x = self.velocity * cos(self.angle)
         # en pygame y aumenta hacia abajo, cambia signo
-        y = -self.velocity * sin(radians(self.angle))
+        y = -self.velocity * sin(self.angle)
 
         new_pos = relative_pos(x, y)
         new_pos = (new_pos[0] + self.rect.topleft[0], new_pos[1] + self.rect.topleft[1])
         self.rect.topleft = new_pos
+        print(self.angle)
 
     def move_fall():
         # animacion cuando le disparan
@@ -50,6 +63,9 @@ class Duck(Element):
     def move_leave():
         # animacion cuando se va
         pass
+
+    def on_draw(self, screen: pygame.Surface):
+        super().on_draw(screen)
 
     def in_sight(x, y) -> bool:
         # true si x y esta dentro de el pato
