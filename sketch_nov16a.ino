7aad32d5ea0ccbdd48dd983fad6af9a136df31a1
JuanJoZP <juanzpjose136@gmail.com>
2022-11-18 12:55:56 -0500

    Add files via upload

---
+++ sketch_nov16a.ino
@@ -0,0 +1,40 @@
+// Librerias I2C para controlar el mpu6050
+// la libreria MPU6050.h necesita I2Cdev.h, I2Cdev.h necesita Wire.h
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+// La dirección del MPU6050 puede ser 0x68 o 0x69, dependiendo 
+// del estado de AD0. Si no se especifica, 0x68 estará implicito
+MPU6050 sensor;
+
+// Valores RAW (sin procesar) del acelerometro  en los ejes x,y,z
+int ax, ay, az;
+
+void setup() {
+  Serial.begin(57600);    //Iniciando puerto serial
+  Wire.begin();           //Iniciando I2C  
+  sensor.initialize();    //Iniciando el sensor
+
+  pinMode(4, INPUT);
+}
+
+void loop() {
+  // Leer las aceleraciones 
+  sensor.getAcceleration(&ax, &ay, &az);
+  //Calcular los angulos de inclinacion:
+  float accel_ang_x=atan(ax/sqrt(pow(ay,2) + pow(az,2)))*(180.0/3.14);
+  float accel_ang_y=atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
+  //Mostrar los angulos separadas por un [tab]
+  if(digitalRead(4) == HIGH) {
+    Serial.print("shot");
+  } else {
+    Serial.print("no-shot");
+  }
+  Serial.print(" ");
+  Serial.print(accel_ang_x); 
+  Serial.print(" ");
+  Serial.println(accel_ang_y);
+  
+  delay(1000/60);
+}
